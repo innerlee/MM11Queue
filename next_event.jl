1e03855df819f16859feaf49b76977c450f175b9
innerlee <363664470@qq.com>
2015-10-07 15:31:55 +0800

    fixed bug.

--- next_event.jl
+++ next_event.jl
@@ -8,14 +8,14 @@ function next_event(
   lambda_inv = 1/lambda
   mu_inv = 1/mu
   total_steps = Int(floor(t_end/delta_t)) # not includs step 0
-  N = falses(total_steps + 1) # state in each step, true for busy
+  N = falses(total_steps) # state in each step, true for busy
   busy = false # init free
 
   #say("= next event: total steps: $total_steps")
 
   step = 1
   while step < total_steps
-    interval = Int(floor((exponential(lambda_inv)/delta_t)))
+    interval = Int(round((exponential(lambda_inv)/delta_t)))
     step_to = step + interval
 
     if step_to > total_steps
@@ -24,14 +24,14 @@ function next_event(
 
     step = step_to
 
-    busytime = Int(floor((exponential(mu_inv)/delta_t)))
+    busytime = Int(round((exponential(mu_inv)/delta_t)))
     step_to = min(step + busytime, total_steps)
 
     for i = step:step_to
       N[i] = true
     end
 
-    step += 1
+    step = step_to + 1
   end
 
   time = [i*delta_t for i=0:total_steps]
